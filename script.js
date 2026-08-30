4a65c5d01d0d47674341d630ba641bbd1d52c811
shantanuparte <shantanuparte23@gmail.com>
2026-08-30 19:50:54 +0530

    redireted the user to main page and added button functionnality

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hello world")
+const butt = document.getElementById("input_button");
+
+function when_clicked(){
+    const input = document.getElementById("input_user_name")
+    const input_value = input.value
+    localStorage.setItem("userId",input_value)
+    console.log("Clicked", input_value);
+    console.log(butt)
+
+    window.location.href = "terminal.html"
+}
+
+butt.addEventListener("click", when_clicked);
+
+
