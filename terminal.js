15b176515c1466dd781355e5cefa38f4f64ba694
shantanuparte <shantanuparte23@gmail.com>
2026-08-30 23:42:08 +0530

    added css and made basic layout. added time

--- terminal.js
+++ terminal.js
@@ -8,8 +8,19 @@ const greet_user = document.getElementById("greet")
 greet_user.textContent = `Hello ${user_name}`
 
 const now = new Date
-const curent_time = now.toLocaleDateString()
+const for_time = new Intl.DateTimeFormat(
+    'en-US',{
+        hour:'2-digit',
+        minute:'2-digit',
+        hour12: true
+    }
+).format(now)
 
 
-console.log(curent_time)
+console.log(for_time)
+
+const time = document.getElementById("time_p")
+
+time.innerHTML = `${for_time}`
+
 
