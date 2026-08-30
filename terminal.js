6d41b4037478b5d1843030f6ff4c34659baa5d26
shantanuparte <shantanuparte23@gmail.com>
2026-08-30 21:41:45 +0530

    changed layout cause i made mistake. also added time but need to change the format

--- terminal.js
+++ terminal.js
@@ -7,5 +7,9 @@ const greet_user = document.getElementById("greet")
 
 greet_user.textContent = `Hello ${user_name}`
 
+const now = new Date
+const curent_time = now.toLocaleDateString()
 
+
+console.log(curent_time)
 
