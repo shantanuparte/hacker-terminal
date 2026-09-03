a85427627569e77b9b3dce88a6eacd974efc3116
shantanuparte <shantanuparte23@gmail.com>
2026-09-03 20:33:41 +0530

    Added main logic

--- terminal.js
+++ terminal.js
@@ -25,5 +25,75 @@ time.innerHTML = `${for_time}`
 
 
 const form = document.getElementById("terminal_form")
+const input = document.getElementById("terminal_input")
+const output = document.getElementById("terminal_output")
 
+
+function print(text){
+    const line = document.createElement("div")
+    line.textContent = text
+
+    output.appendChild(line)
+
+    output.scrollTop = output.scrollHeight
+
+}
+
+function printCommand(command){
+    const line = document.createElement("div")
+    line.innerHTML = `<span class="prompt">&git;</span> ${command}`
+
+    output.appendChild(line)
+
+}
+
+form.addEventListener("submit", function(event){
+    event.preventDefault()
+
+    const command = input.value.trim()
+
+    if(command === ""){
+        return
+    }
+
+    printCommand(command)
+
+    input.value = ""
+
+    handleCommand(command)
+
+})
+
+function handleCommand(command){
+    switch(command.toLowerCase()){
+
+        case "help":
+            print("Avaliable commands: ")
+            print("  help       -Slow avaliable commands")
+            print("  clear      -Clear terminal")
+            print("  about      -About NightTerminal")
+            print("  echo       -Print text");
+            break
+
+        case "clear":
+             output.innerHTML = ""
+             break
+
+        case "about":
+            print("NightTerminal")
+            print("A Web-Based terminal interface")
+            break
+
+        default:
+            if (command.startsWith("echo")){
+
+                const text = command.substring(5)
+                print(text)
+
+            }
+            else{
+                print(`Command not found: ${command}`)
+            }
+    }
+}
 
